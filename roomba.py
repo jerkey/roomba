f25ff88a52eff6b91d8e39c740c4cf875c8ccc8a
jerkey <jerkey@halo.nu>
2015-03-01 16:35:50 -0800

    now it drives

--- roomba.py
+++ roomba.py
@@ -1,9 +1,11 @@
+import curses
 import sys
 import struct
 import serial
 import time
 
-serialPort = '/dev/ttyUSB0'
+#serialPort = '/dev/ttyUSB0'
+serialPort = '/dev/tty.SLAB_USBtoUART'
 baudRate = 57600
 
 class robot:
@@ -21,29 +23,60 @@ class robot:
     return #self.com.readlines()
 
   def go(self, velocity, radius):
+    print ' bot.go('+str(velocity)+','+str(radius)+')'
     self.com.flushInput()
     v = struct.pack(">h",velocity)
     r = struct.pack(">h",radius)
     cmd = ''.join([chr(137),v[0],v[1],r[0],r[1]])
+    #cmd = ''.join([chr(137),v[1],v[0],r[1],r[0]])
     self.com.write(cmd+"\r\n")
     return
 
   def read1line(self):
     return self.com.readline()
 
+
 def main(*arg):
+    screen = curses.initscr()  #we're not in kansas anymore
+    curses.noecho()
+    curses.curs_set(0)
+    screen.timeout(0)
+    screen.scrollok(True)
+    screenSize = screen.getmaxyx()
+    midX = int(screenSize[1]/2)
+    screen.keypad(1)  #nothing works without this
     bot = robot()
     print bot.init(serialPort , baudRate)
     # bot.cmnd(''.join(map(chr, [128,132,139,2,0,0])))
     fullmode = [128,132]
-    song = [140,0,4,62,12,66,12,69,12,74,36]
+    #song = [140,0,4,62,12,66,12,69,12,74,36]
+    song = [140,0,1,62,12]
     playsong = [141,0]
     drive = [137,0,00,128,0]
     bot.cmnd(''.join(map(chr, fullmode+song+playsong)))
-    bot.go(200,0)
-    time.sleep(2)
     bot.go(0,0)
-
+    press = screen.getch()
+    while press != ord('q'):
+      press = screen.getch()
+      while press == -1:
+        press = screen.getch()
+      bot.cmnd(''.join(map(chr, song+playsong)))
+      if press == ord('j'): #back
+        bot.go(-200,-32768)
+        time.sleep(1)
+        bot.go(0,0)
+      if press == ord('k'): #fwd
+        bot.go(200,-32768)
+        time.sleep(1)
+        bot.go(0,0)
+      if press == ord('h'): #left
+        bot.go(200,1)
+        time.sleep(0.5)
+        bot.go(0,0)
+      if press == ord('l'): #right
+        bot.go(200,-1)
+        time.sleep(0.5)
+        bot.go(0,0)
 
 if __name__=='__main__':
     sys.exit(main(*sys.argv))
